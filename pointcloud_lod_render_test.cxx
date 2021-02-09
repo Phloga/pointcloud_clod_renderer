15a07d0ca5ca9f81bf23cdf51c7f4486291c8290
NicoKlaus <nico.volkens@mailbox.tu-dresden.de>
2021-02-09 12:27:26 +0100

    initial commit

---
+++ pointcloud_lod_render_test.cxx
@@ -0,0 +1,231 @@
+#include <cgv/base/base.h>
+#include "pointcloud_lod_render_test.h"
+#include <cgv_gl/gl/gl.h>
+#include <cgv/gui/file_dialog.h>
+#include <random>
+#include <chrono>
+#include <numeric>
+#include <random>
+
+using namespace std;
+using namespace cgv::base;
+using namespace cgv::signal;
+using namespace cgv::type;
+using namespace cgv::gui;
+using namespace cgv::data;
+using namespace cgv::utils;
+using namespace cgv::render;
+
+pointcloud_lod_render_test::pointcloud_lod_render_test() {
+	set_name("pointcloud_lod_render_test");
+
+	source_prs.measure_point_size_in_pixel = false;
+	source_prs.point_size = 1.00f;
+	source_prs.blend_width_in_pixel = 1.0f;
+	source_prs.blend_points = true;
+
+	source_srs.measure_point_size_in_pixel = false;
+	source_srs.point_size = 1.00f;
+	source_srs.blend_width_in_pixel = 1.0f;
+	source_srs.blend_points = true;
+	source_srs.illumination_mode = cgv::render::IM_TWO_SIDED;
+
+	rot_intensity = 0.2f;
+	trans_intensity = 0.1;
+	lrs.line_width = 1.0f;
+	rcrs.radius = 0.001f;
+}
+
+bool pointcloud_lod_render_test::self_reflect(cgv::reflect::reflection_handler & rh)
+{
+	return rh.reflect_member("ply_path", ply_path);
+}
+
+void pointcloud_lod_render_test::on_set(void * member_ptr)
+{
+}
+
+void pointcloud_lod_render_test::on_register()
+{
+}
+
+void pointcloud_lod_render_test::unregister()
+{
+}
+
+bool pointcloud_lod_render_test::init(cgv::render::context & ctx)
+{
+	ctx.set_bg_clr_idx(3);
+	ctx.set_bg_color(0, 0, 0, 0.9);
+	cgv::render::view* view_ptr = find_view_as_node();
+	if (view_ptr) {
+		view_ptr->set_view_up_dir(vec3(0, -1, 0));
+		view_ptr->set_focus(vec3(0, 0, 0));
+	}
+	//cgv::render::ref_point_renderer(ctx, 1);
+	cgv::render::ref_surfel_renderer(ctx, 1);
+	cgv::render::ref_rounded_cone_renderer(ctx, 1);
+	cp_renderer.init(ctx);
+	ctx.set_bg_color(0.7, 0.7, 0.8, 1.0);
+	return true;
+}
+
+void pointcloud_lod_render_test::prepare_point_cloud(point_cloud& pc)
+{
+
+}
+
+void pointcloud_lod_render_test::draw(cgv::render::context & ctx)
+{
+	ctx.push_modelview_matrix();
+	cp_renderer.set_render_style(cp_style);
+
+	if (source_pc.get_nr_points() > 0) {
+		if (renderer_out_of_date) {
+			vec4 color(1.0, 0.0, 0.0, 1.0);
+			vector<point_cloud::Pnt> P(source_pc.get_nr_points());
+			vector<point_cloud::Clr> C(source_pc.get_nr_points());
+			vector<short> LODS(source_pc.get_nr_points());
+			for (int i = 0; i < source_pc.get_nr_points(); ++i) {
+				P[i] = source_pc.pnt(i);
+				C[i] = source_pc.clr(i);
+
+				LODS[i] = rand() % 8; //random lods for testing
+			}
+			//vector<cgv::render::render_types::rgba> colors(source_pc.get_nr_points(), rgba(color.x(), color.y(), color.z(), 0.f));
+
+			cp_renderer.set_positions(ctx, P);
+			cp_renderer.set_colors(ctx, C);
+			cp_renderer.generate_lods();
+			//cp_renderer.set_lods(LODS);
+			renderer_out_of_date = false;
+		}
+		if (cp_renderer.enable(ctx))
+			cp_renderer.draw(ctx, 0, source_pc.get_nr_points());
+	}
+	ctx.pop_modelview_matrix();
+
+	//draw_point_cloud(ctx, source_pc,vec4(1.0,0.0,0.0,1.0));
+
+	if (view_find_point_cloud) {
+		find_pointcloud(ctx);
+		view_find_point_cloud = false;
+	}
+}
+
+void pointcloud_lod_render_test::find_pointcloud(cgv::render::context & ctx)
+{
+	cgv::render::view* view_ptr = find_view_as_node();
+	if (view_ptr) {
+		const point_cloud_types::Box& sb = source_pc.box();
+		point_cloud_types::Box aabb(sb);
+
+		view_ptr->set_focus(aabb.get_center());
+		
+		view_ptr->move(view_ptr->get_depth_of_focus()-1.0);
+		
+	}
+
+	
+}
+
+void pointcloud_lod_render_test::clear(cgv::render::context & ctx)
+{
+	//cgv::render::ref_point_renderer(ctx, -1);
+	cgv::render::ref_surfel_renderer(ctx, -1);
+	cgv::render::ref_rounded_cone_renderer(ctx, -1);
+}
+
+bool pointcloud_lod_render_test::handle(cgv::gui::event & e)
+{
+	return false;
+}
+
+void pointcloud_lod_render_test::stream_help(std::ostream & os)
+{
+}
+
+void pointcloud_lod_render_test::create_gui()
+{
+	add_decorator("Point cloud", "heading", "level=1");
+	connect_copy(add_button("load point cloud")->click, rebind(this, &pointcloud_lod_render_test::on_load_point_cloud_cb));
+	connect_copy(add_button("clear point cloud")->click, rebind(this, &pointcloud_lod_render_test::on_clear_point_cloud_cb));
+	connect_copy(add_button("randomize position")->click, rebind(this, &pointcloud_lod_render_test::on_randomize_position_cb));
+	add_member_control(this, "rotation intensity", rot_intensity, "value_slider", "min=0.01;max=1.0;log=false;ticks=true");
+	add_member_control(this,"translation intensity", trans_intensity, "value_slider", "min=0.01;max=1.0;log=false;ticks=true");
+	connect_copy(add_button("find point cloud")->click, rebind(this, &pointcloud_lod_render_test::on_reg_find_point_cloud_cb));
+
+	add_decorator("point cloud", "heading", "level=2");
+	connect_copy(add_control("Point size", source_srs.point_size, "value_slider", "min=0.01;max=3.0;log=false;ticks=true")->value_change, rebind(this, &pointcloud_lod_render_test::on_point_cloud_style_cb));
+
+
+	///rounded_cone_render
+	if (begin_tree_node("cone render style", rcrs)) {
+		align("\a");
+		add_gui("render_style", rcrs);
+		align("\b");
+		end_tree_node(rcrs);
+	}
+	
+	if (begin_tree_node("Surfel Rendering", source_srs, false)) {
+		align("\a");
+		add_gui("surfel_style", source_srs);
+		align("\b");
+		end_tree_node(source_srs);
+	}
+	
+	if (begin_tree_node("clod_render_style", cp_style, false)) {
+		align("\a");
+		add_gui("clod style", cp_style);
+		align("\b");
+		end_tree_node(cp_style);
+	}
+}
+
+void pointcloud_lod_render_test::timer_event(double t, double dt)
+{
+}
+
+void pointcloud_lod_render_test::on_load_point_cloud_cb()
+{
+	std::string fn = cgv::gui::file_open_dialog("source point cloud", "Point cloud files (obj,ply):*.obj;*.ply");
+	source_pc.read(fn);
+	renderer_out_of_date = true;
+	post_redraw();
+}
+
+
+void pointcloud_lod_render_test::on_clear_point_cloud_cb()
+{
+	source_pc.clear();
+	crs_srs_pc.clear();
+	renderer_out_of_date = true;
+	post_redraw();
+}
+
+void pointcloud_lod_render_test::on_randomize_position_cb()
+{
+	uniform_real_distribution<float> angle_distribution(0.f, 3.142f);
+	uniform_real_distribution<float> direction_distribution(0.f, 0.05f);
+	random_device rng;
+	float angle = rot_intensity*angle_distribution(rng);
+	source_pc.rotate(cgv::math::quaternion<float>(normalize(vec3(direction_distribution(rng), direction_distribution(rng), direction_distribution(rng))), angle));
+	source_pc.translate(trans_intensity*vec3(direction_distribution(rng), direction_distribution(rng), direction_distribution(rng)));
+	renderer_out_of_date = true;
+	post_redraw();
+}
+
+void pointcloud_lod_render_test::on_reg_find_point_cloud_cb()
+{
+	view_find_point_cloud = true;
+	post_redraw();
+}
+
+void pointcloud_lod_render_test::on_point_cloud_style_cb()
+{
+	post_redraw();
+}
+
+#include "lib_begin.h"
+
+extern CGV_API object_registration<pointcloud_lod_render_test> kc_or("");
---
+++ pointcloud_lod_render_test.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <cgv/base/node.h>
+#include <cgv/math/fvec.h>
+#include <cgv/media/color.h>
+#include <cgv/gui/event_handler.h>
+#include <cgv/gui/provider.h>
+#include <cgv/data/data_view.h>
+#include <cgv/render/drawable.h>
+#include <cgv/render/shader_program.h>
+#include <cgv/render/texture.h>
+#include <cgv/render/render_types.h>
+#include <cgv_gl/point_renderer.h>
+#include <cgv_gl/line_renderer.h>
+#include <cgv_gl/rounded_cone_renderer.h>
+#include <cgv_gl/surfel_renderer.h>
+#include <cgv_gl/clod_point_renderer.h>
+
+#include <point_cloud.h>
+
+#include <string>
+#include <mutex>
+#include <future>
+
+#include "lib_begin.h"
+
+class pointcloud_lod_render_test :
+	public cgv::base::node,
+	public cgv::render::drawable,
+	public cgv::gui::event_handler,
+	public cgv::gui::provider
+{
+public:
+	pointcloud_lod_render_test();
+
+	/// overload to return the type name of this object. By default the type interface is queried over get_type.
+	std::string get_type_name() const { return "pointcloud_lod_render_test"; }
+
+	bool self_reflect(cgv::reflect::reflection_handler& rh);
+
+	void on_set(void* member_ptr);
+	
+	void on_register();
+	
+	void unregister();
+
+	/// adjust view
+	bool init(cgv::render::context& ctx);
+	/// overload to draw the content of this drawable
+	void draw(cgv::render::context& ctx);
+	///
+	void find_pointcloud(cgv::render::context& ctx);
+	///
+	void clear(cgv::render::context& ctx);
+	/// 
+	bool handle(cgv::gui::event& e);
+	/// 
+	void stream_help(std::ostream& os);
+	///
+	void create_gui();
+
+protected:
+	void timer_event(double t, double dt);
+
+	void on_load_point_cloud_cb();
+	void on_clear_point_cloud_cb();
+	void on_randomize_position_cb();
+	void on_reg_find_point_cloud_cb();
+	void on_point_cloud_style_cb();
+	void prepare_point_cloud(point_cloud& pc);
+
+private:
+	std::string ply_path;
+	point_cloud source_pc, crs_srs_pc;
+	cgv::render::point_render_style source_prs;
+	cgv::render::surfel_render_style source_srs;
+	cgv::render::line_render_style lrs;
+	cgv::render::rounded_cone_render_style rcrs;
+
+	float rot_intensity;
+	float trans_intensity;
+	bool view_find_point_cloud;
+	bool renderer_out_of_date = true;
+
+	cgv::render::clod_point_renderer cp_renderer;
+	cgv::render::clod_point_render_style cp_style;
+};
+
+#include <cgv/config/lib_end.h>
